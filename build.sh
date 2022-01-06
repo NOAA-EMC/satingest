3faeb92f80f1762a0481bbd425e6b6c9dc25ef91
Shelley Melchior <Shelley.Melchior@noaa.gov>
2022-01-06 03:07:16 +0000

    Squashed commit of the following:
    commit 8c09cf4bb04d831464e05a093be2a85d3db6eefa
    Author: Shelley Melchior <Shelley.Melchior@noaa.gov>
    Date:   Thu Jan 6 03:03:44 2022 +0000
    
        Correcting version numbers for bufr_tranjb.
        Removing local load of bufr_tranjb module.
    
    commit 4fc62fa193ddedeeb5d64fed165df972bc7b0651
    Author: Rahul Mahajan <aerorahul@users.noreply.github.com>
    Date:   Wed Jan 5 21:48:04 2022 -0500
    
        adopt for current NCO working practices per discussion w/ StevenEarle et al (#6)

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -eux
 
-INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
-INSTALL_PREFIX=${INSTALL_PREFIX:-"../install"}
-
 # Location of PWD and package source directory.
 pkg_root=`dirname $(readlink -f $0)`
+
+INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-"$pkg_root/install"}
 
 target=$(echo $INSTALL_TARGET | tr [:upper:] [:lower:])
 if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
@@ -16,8 +16,6 @@ if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
   module use $pkg_root/modulefiles
   module load satingest_$target
   module list
-  module unload bufr
-  export bufr_ROOT=/lfs/h2/emc/obsproc/noscrub/Shelley.Melchior/install/install/bufr
   set -x
 fi
 
@@ -29,5 +27,23 @@ mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_INSTALL_BINDIR=exec ..
 make -j ${BUILD_JOBS:-6} VERBOSE=${BUILD_VERBOSE:-}
 make install
+
+#############################################################################
+# This section to be removed when NCO is comfortable with the typical
+# `cmake`, `make` and `make install` process.
+# To abide by current NCO working practices,
+# manually copy compiled executables and fix files from `$INSTALL_PREFIX/`
+# directory to `pkg_root` and then remove `$INSTALL_PREFIX/`
+mkdir -p $pkg_root/exec
+cp -f $INSTALL_PREFIX/exec/*                     $pkg_root/exec/
+cp -f $INSTALL_PREFIX/fix/bufr_hirsrtcf_ibm.dat  $pkg_root/fix/
+cp -f $INSTALL_PREFIX/fix/bufr_lowtopog.dat      $pkg_root/fix/
+cp -f $INSTALL_PREFIX/fix/nesdis.lstags_transsmi $pkg_root/fix/
+rm -rf $INSTALL_PREFIX
+#############################################################################
+
+# Remove build directory upon successfull build and install
+cd $pkg_root
+rm -rf build
 
 exit 0
