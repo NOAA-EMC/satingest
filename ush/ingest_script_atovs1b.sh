4919a3e75201ccd94f39ca81a6f2134bff3af1dd
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 11:11:12 -0500

    Update ingest_script_atovs1b.sh

--- ush/ingest_script_atovs1b.sh
+++ ush/ingest_script_atovs1b.sh
@@ -68,6 +68,8 @@
 #                            files.  Updated some existing comments.
 # 2018-12-08  Yangrong Ling  Added ability to handle METOP-3(C) satellite.
 # 2021-12-19  Sudhir Nadiga  Modified to use bufr_tranjb module variables.
+# 2022-01-18  S. Stegall     Replaced $DATA/ before calling utility scripts and instead 
+#                            used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_script_atovs1b.sh  <bufrtable>  <raw_file>
