d4dcc525635db6a0db7229c2880790d0435b7572
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 11:15:35 -0500

    Update ingest_script_omi.sh

--- ush/ingest_script_omi.sh
+++ ush/ingest_script_omi.sh
@@ -34,6 +34,8 @@
 #                              portion of the code to deal with incomplete files
 #                              has been commented out.
 # 2021-12-19  Sudhir Nadiga    Modified to use bufr_tranjb module variables.
+# 2022-01-18  S. Stegall       Replaced $DATA/ before calling utility scripts and instead 
+#                              used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_script_omi.sh  <bufrtable>  <raw_file>
