deb291a497d6d8bc0e17229a1ea0a02419d746bc
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 09:11:28 -0500

    Update existday.sh

--- scripts/existday.sh
+++ scripts/existday.sh
@@ -85,7 +85,7 @@
 #        from model script existore.sh.ecf. It is set here to avoid an "unset
 #        variable" error in ingest_get.sh when that script is executed
 #        downstream from this model script (existday.sh.ecf).
-#
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 # Usage: existday.sh.ecf
 #
