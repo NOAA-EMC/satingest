138177af9dc3e5e0e9352f1c67918a2217ff56a6
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 11:17:46 -0500

    Update ingest_snodepgr.sh

--- ush/ingest_snodepgr.sh
+++ ush/ingest_snodepgr.sh
@@ -69,6 +69,8 @@
 #                         References to SENDDBN_GB2 were removed due to
 #                         discontinuation of deliberate separation of grib and
 #                         grib2 alerts.
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead 
+#                         used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 # Usage: ingest_snodepgr.sh  <dummy>  <raw_file>
 #
