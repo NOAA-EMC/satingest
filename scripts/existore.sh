b0acdccadbe366c8faa38a7be4f4066fcbfa6b7f
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 09:45:51 -0500

    Update existore.sh

--- scripts/existore.sh
+++ scripts/existore.sh
@@ -241,7 +241,8 @@
 #                 LGYCLD2 (the primary) which continues to pull these same files
 #                 from the LaRC typhoon server.
 #
-# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead used $UTILROOT/ush/ to properly leverage the prod_util module.
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead 
+#         used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 # Usage: existore.sh.ecf
 #
