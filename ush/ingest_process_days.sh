9626b240bd370ec5e4d510afa218702262cdde61
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 09:33:31 -0500

    Update ingest_process_days.sh

--- ush/ingest_process_days.sh
+++ ush/ingest_process_days.sh
@@ -66,6 +66,7 @@
 #     representing the directory path to the executables.  Added information to
 #     docblock and new comments.  Updated some existing comments.
 # 2018-12-06  Y. Ling  Updated to run on phase 3 machine.
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_process_days.sh
