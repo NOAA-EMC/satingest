b0a7fd65a002c7540799852f1fa1192145b659db
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 11:12:17 -0500

    Update ingest_script_mls.sh

--- ush/ingest_script_mls.sh
+++ ush/ingest_script_mls.sh
@@ -32,6 +32,8 @@
 #                              representing the directory path to the
 #                              executables.  Updated some existing comments.
 # 2021-12-19  Sudhir Nadiga    Modified to use bufr_tranjb module variables.
+# 2022-01-18  S. Stegall       Replaced $DATA/ before calling utility scripts and instead 
+#                              used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_script_mls.sh  <bufrtable>  <raw_file>
