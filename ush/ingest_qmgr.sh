29160c4111118f18c295469ddd399a6d7f8e856d
Shelley Melchior <Shelley.Melchior@noaa.gov>
2022-01-18 16:41:11 +0000

    Modified formatting in doc blocks

--- ush/ingest_qmgr.sh
+++ ush/ingest_qmgr.sh
@@ -45,8 +45,9 @@
 #                            in bjobs for $OWNER (since there is sometimes a
 #                            lag).
 #  2021-10-15  Nadiga     -  changed bjobs to qstat
-#  2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead 
-#      used $UTILROOT/ush/ to properly leverage the prod_util module.
+#  2022-01-18  S. Stegall -  Replaced $DATA/ before calling utility scripts and
+#                            instead used $UTILROOT/ush/ to properly leverage the
+#                            prod_util module.
 #
 
 set -x
