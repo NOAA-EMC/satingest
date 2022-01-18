ec09eadf63d64f840c84285805800151465da3b6
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 11:18:54 -0500

    Update ingest_translate_orbits.sh

--- ush/ingest_translate_orbits.sh
+++ ush/ingest_translate_orbits.sh
@@ -143,6 +143,8 @@
 #     and do not suffer from the problem of incompleteness, so the lines to process
 #     incomplete files have been commented out.
 # 2021-12-19  S. Nadiga Modified to use bufr_tranjb module variables.
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead 
+#      used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_translate_orbits.sh
