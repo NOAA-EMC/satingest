50f2ca1e1d1e75b6daf129ee780c8d0da65a54d0
Shelley Melchior <Shelley.Melchior@noaa.gov>
2022-01-18 15:33:19 +0000

    Modified formatting in doc blocks

--- scripts/existore.sh
+++ scripts/existore.sh
@@ -240,7 +240,6 @@
 #                 server in a different directory than the same files pulled by
 #                 LGYCLD2 (the primary) which continues to pull these same files
 #                 from the LaRC typhoon server.
-#
 # 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead 
 #         used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
