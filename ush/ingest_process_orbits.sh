d3d4cb58c50ec776ddd107d7f7cf9447b4dc248f
SteveStegall-NOAA <92433130+SteveStegall-NOAA@users.noreply.github.com>
2022-01-18 09:40:32 -0500

    Update ingest_process_orbits.sh

--- ush/ingest_process_orbits.sh
+++ ush/ingest_process_orbits.sh
@@ -179,6 +179,7 @@
 #     one of the expected patterns for either gzip or bzip2 compression, then
 #     the file is skipped.  This is also the case if the execution of gunzip
 #     or bunzip2 fails.
+# 2022-01-18  S. Stegall  Replaced $DATA/ before calling utility scripts and instead used $UTILROOT/ush/ to properly leverage the prod_util module.
 #
 #
 # Usage: ingest_process_orbits.sh
